78130c91fdfc590419b6647d2b45f1d61a52ea5f
Kyle Oros <kyleblissoros@gmail.com>
2023-09-20 19:28:01 -0700

    changes

--- Assignment/index.js
+++ Assignment/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const logoQuestions = ([
         {
             type: 'input',
-            name: 'name',
+            name: 'company',
             message: "What is your company name? Three letter maximum.",
         },
         {
@@ -16,21 +16,25 @@ const logoQuestions = ([
             type: 'list',
             name: 'shape',
             message: 'What shape would you like your logo to be in?',
-            choices: ['circle', 'sqaure', 'triangle'],
+            choices: ['circle', 'square', 'triangle'],
         }
     ]);
 
 inquirer.prompt(logoQuestions).then((answers) => {
-    const name = answers.name;
+    const company = answers.company;
     const color = answers.color;
     const shape = answers.shape;
 
     const svg = `<svg viewBox = "0 0 200 200">
     <${shape} fill="${color}" width="200" height="200" />
-    <text x="50" y="50" text-anchor="middle" font-size="24" fill="black">${name}</text>
+    <text x="50" y="50" text-anchor="middle" font-size="24" fill="black">${company}</text>
     </svg>`;
+
+    fs.writeFileSync('logo.svg', svg);
+    console.log("Logo generated.");
 });
 
+/*
 fs.writeFileSync('logo.svg', svg);
 
 const svg2png = require('svg2png');
@@ -45,3 +49,4 @@ svg2png(svgFile, pngFile)
     .catch((err) => {
         console.error(err);
     });
+*/
